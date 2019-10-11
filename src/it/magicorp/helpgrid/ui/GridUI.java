dec2e4b2b6d8c2ad1a0f0c85ce88c25106259fd7
MagicalMouse39 <magicalmouse39@gmail.com>
2019-10-11 10:25:40 +0200

    Update
    Added Icons

--- src/it/magicorp/helpgrid/ui/GridUI.java
+++ src/it/magicorp/helpgrid/ui/GridUI.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -71,7 +72,7 @@ public class GridUI extends JFrame implements KeyListener
 		this.messageArea.setBackground(this.isDarkMode ? new Color(33, 33, 40) : new Color(200, 200, 200));
 	}
 	
-	private int bw = 100;
+	private int bw = 200;
 	
 	private int col = 0, row = 0, maxRow = 0;
 	
@@ -81,7 +82,7 @@ public class GridUI extends JFrame implements KeyListener
 	
 	private void addButtons()
 	{
-		JButton helpBtn = new JButton(this.lang == Language.ENGLISH ? "Help" : "Aiuto");
+		JButton helpBtn = new JButton(this.lang == Language.ENGLISH ? "Help" : "Aiuto", new ImageIcon("Icons/help.png"));
 		helpBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -103,7 +104,7 @@ public class GridUI extends JFrame implements KeyListener
 		this.buttons.add(helpBtn);
 		this.names.put("Help", "Aiuto");
 	
-		JButton darkModeBtn = new JButton("Dark Mode " + (this.isDarkMode ? "[ON]" : "[OFF]"));
+		JButton darkModeBtn = new JButton("Dark Mode " + (this.isDarkMode ? "[ON]" : "[OFF]"), new ImageIcon("Icons/nightmode.png"));
 		
 		darkModeBtn.addActionListener(new ActionListener()
 		{
@@ -116,7 +117,7 @@ public class GridUI extends JFrame implements KeyListener
 		});
 		this.buttons.add(darkModeBtn);
 		
-		JButton yesBtn = new JButton(this.lang == Language.ENGLISH ? "Yes" : "Si");
+		JButton yesBtn = new JButton(this.lang == Language.ENGLISH ? "Yes" : "Si", new ImageIcon("Icons/thumbup.png"));
 		yesBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -128,7 +129,7 @@ public class GridUI extends JFrame implements KeyListener
 		this.buttons.add(yesBtn);
 		this.names.put("Yes", "Si");
 		
-		JButton noBtn = new JButton("No");
+		JButton noBtn = new JButton("No", new ImageIcon("Icons/thumbdown.png"));
 		noBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -139,7 +140,7 @@ public class GridUI extends JFrame implements KeyListener
 		});
 		this.buttons.add(noBtn);
 		
-		JButton bathroomBtn = new JButton(this.lang == Language.ENGLISH ? "Bathroom" : "Bagno");
+		JButton bathroomBtn = new JButton(this.lang == Language.ENGLISH ? "Bathroom" : "Bagno", new ImageIcon("Icons/bathroom.png"));
 		bathroomBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -152,7 +153,7 @@ public class GridUI extends JFrame implements KeyListener
 		this.buttons.add(bathroomBtn);
 		this.names.put("Bathroom", "Bagno");
 		
-		JButton foodBtn = new JButton(this.lang == Language.ENGLISH ? "Food" : "Cibo");
+		JButton foodBtn = new JButton(this.lang == Language.ENGLISH ? "Food" : "Cibo", new ImageIcon("Icons/food.png"));
 		foodBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -165,7 +166,7 @@ public class GridUI extends JFrame implements KeyListener
 		this.buttons.add(foodBtn);
 		this.names.put("Food", "Cibo");
 		
-		JButton drinkBtn = new JButton(this.lang == Language.ENGLISH ? "Drink" : "Acqua");
+		JButton drinkBtn = new JButton(this.lang == Language.ENGLISH ? "Drink" : "Acqua", new ImageIcon("Icons/water.png"));
 		drinkBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -178,7 +179,59 @@ public class GridUI extends JFrame implements KeyListener
 		this.buttons.add(drinkBtn);
 		this.names.put("Drink", "Acqua");
 		
-		JButton langBtn = new JButton(this.lang == Language.ENGLISH ? "Cambia a ITA" : "Change to ENG");
+		JButton tooHot = new JButton(this.lang == Language.ENGLISH ? "Too Hot" : "Troppo Caldo", new ImageIcon("Icons/toohot.png"));
+		tooHot.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				GridUI.this.messageArea.setText(GridUI.this.lang == Language.ENGLISH ? "Too Hot!" : "Troppo Caldo!");
+				SoundUtils.play(1);
+			}
+		});
+		this.buttons.add(tooHot);
+		this.names.put("Too Hot", "Troppo Caldo");
+		
+		JButton tooCold = new JButton(this.lang == Language.ENGLISH ? "Too Cold" : "Troppo Freddo");
+		tooCold.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				GridUI.this.messageArea.setText(GridUI.this.lang == Language.ENGLISH ? "Too Cold!" : "Troppo Freddo!");
+				SoundUtils.play(1);
+			}
+		});
+		this.buttons.add(tooCold);
+		this.names.put("Too Cold", "Troppo Freddo");
+		
+		JButton lightOn = new JButton(this.lang == Language.ENGLISH ? "Light ON" : "Accendi Luce");
+		lightOn.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				GridUI.this.messageArea.setText(GridUI.this.lang == Language.ENGLISH ? "Turn the light ON" : "Accendi la Luce!");
+				SoundUtils.play(1);
+			}
+		});
+		this.buttons.add(lightOn);
+		this.names.put("Light ON", "Accendi Luce");
+		
+		JButton lightOff = new JButton(this.lang == Language.ENGLISH ? "Light ON" : "Accendi Luce");
+		lightOff.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				GridUI.this.messageArea.setText(GridUI.this.lang == Language.ENGLISH ? "Turn the light OFF" : "Spegni la Luce!");
+				SoundUtils.play(1);
+			}
+		});
+		this.buttons.add(lightOff);
+		this.names.put("Light OFF", "Spegni Luce");
+		
+		/*JButton langBtn = new JButton(this.lang == Language.ENGLISH ? "Cambia a ITA" : "Change to ENG");
 		langBtn.addActionListener(new ActionListener()
 		{
 			@Override
@@ -203,7 +256,7 @@ public class GridUI extends JFrame implements KeyListener
 			}
 		});
 		this.buttons.add(langBtn);
-		this.names.put("Cambia a ITA", "Change to ENG");
+		this.names.put("Cambia a ITA", "Change to ENG");*/
 		
 		/*
 		 * Buttons property setter
@@ -329,19 +382,19 @@ public class GridUI extends JFrame implements KeyListener
 		
 		if (e.getKeyCode() == KeyEvent.VK_UP)
 			if (this.hoverPane.getY() > 10)
-				this.hoverPane.setBounds(this.hoverPane.getX(), this.hoverPane.getY() - 110, this.bw, this.bw);
+				this.hoverPane.setBounds(this.hoverPane.getX(), this.hoverPane.getY() - (this.bw + 10), this.bw, this.bw);
 
 		if (e.getKeyCode() == KeyEvent.VK_LEFT)
 			if (this.hoverPane.getX() > 10)
-				this.hoverPane.setBounds(this.hoverPane.getX() - 110, this.hoverPane.getY(), this.bw, this.bw);
+				this.hoverPane.setBounds(this.hoverPane.getX() - (this.bw + 10), this.hoverPane.getY(), this.bw, this.bw);
 		
 		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-			if (this.hoverPane.getY() / 110 < this.col && !(this.hoverPane.getY() / 110 + 1 == this.col && this.hoverPane.getX() / 110 >= this.row))
-				this.hoverPane.setBounds(this.hoverPane.getX(), this.hoverPane.getY() + 110, this.bw, this.bw);
+			if (this.hoverPane.getY() / (this.bw + 10) < this.col && !(this.hoverPane.getY() / (this.bw + 10) + 1 == this.col && this.hoverPane.getX() / (this.bw + 10) >= this.row))
+				this.hoverPane.setBounds(this.hoverPane.getX(), this.hoverPane.getY() + (this.bw + 10), this.bw, this.bw);
 		
 		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-			if ((this.hoverPane.getY() / 110 < this.col && this.hoverPane.getX() / 110 < this.maxRow - 1) || (this.hoverPane.getY() / 110 == this.col && this.hoverPane.getX() / 110 < this.row - 1))
-				this.hoverPane.setBounds(this.hoverPane.getX() + 110, this.hoverPane.getY(), this.bw, this.bw);
+			if ((this.hoverPane.getY() / (this.bw + 10) < this.col && this.hoverPane.getX() / (this.bw + 10) < this.maxRow - 1) || (this.hoverPane.getY() / (this.bw + 10) == this.col && this.hoverPane.getX() / (this.bw + 10) < this.row - 1))
+				this.hoverPane.setBounds(this.hoverPane.getX() + (this.bw + 10), this.hoverPane.getY(), this.bw, this.bw);
 	}
 
 	@Override
